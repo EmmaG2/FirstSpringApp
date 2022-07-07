14719732540f22ae6fa251c545fc040ad119aaec
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-07-07 18:33:52 -0500

    first commit ✅

---
+++ src/main/java/com/springlearn/clase1/Taller1Application.java
@@ -0,0 +1,13 @@
+package com.springlearn.clase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearn/clase1/Taller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.clase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
